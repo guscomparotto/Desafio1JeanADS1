375a195b4f1fb19b18a24a56db052e2c777534dc
imbard <nicota1234@gmail.com>
2022-11-30 21:28:04 -0300

    :sparkles: feat: new branch

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'contatos'
+
+mysql = MySQL(app)
+
 @app.route('/')
 def home1 ():
     return  render_template('index.html')
@@ -8,10 +18,37 @@ def home1 ():
 def home ():
     return  render_template('index.html')
 
-@app.route('/contato')
+#contato sofre alteração devido ao formulário
+@app.route('/contato', methods=['GET', 'POST'])
 def contatos ():
+    if request.method == "POST":  #se o usuário requisitar o método post
+        email = request.form['email'] #pega a informação que um usuário está preenchendo
+        assunto = request.form['assunto']
+        descricao = request.form['descricao']
+
+        cur = mysql.connection.cursor() #abre a conexão com o banco
+        cur.execute('insert into contatos (email, assunto, descricao) values (%s, %s, %s)', (email, assunto, descricao)) #executa o insert into
+        #%s = string  
+        #primeiro campo assunto descricao = variação de carcteres na tabela
+        #segundo campo = onde será inserido os dados
+
+        mysql.connection.commit() #commita as informações do usuário
+        cur.close() #fecha o post
+
+        return 'sucesso'
     return  render_template('contato.html')
 
 @app.route('/quem-somos')
 def quemsomos ():
     return  render_template('quem-somos.html')
+
+@app.route('/users')
+def users():
+    cur = mysql.connection.cursor()
+    users = cur.execute('SELECT * FROM contatos')
+    
+    if users > 0: #só entra no fetchall caso haja pelo menos um usuário
+        userDetails = cur.fetchall()
+
+    return render_template('user.html', userDetails=userDetails) #tabela #for html
+    
